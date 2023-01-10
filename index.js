a85d2ef2d133c19727a1e4260e9955e716602256
Mike <nhj25252@gmail.com>
2023-01-10 13:45:41 +0900

    Get player's choice

--- index.js
+++ index.js
@@ -10,4 +10,39 @@ function getComputerChoice() {
   } else getRandomInt() === 2;
   return "Scissors";
 }
-//yay!
+
+let getPlayerChoice = prompt();
+
+function playerChoice() {
+    if (getPlayerChoice === "Rock") {
+      return 0;
+    } else if (getPlayerChoice === "Paper") {
+      return 1;
+    } else (getPlayerChoice === "Scissors");
+    return 2; 
+    }
+  
+
+/*
+if player is rock 
+    if computer is rock
+    return 'tie'
+if else computer is paper
+    return plalyer loses 
+    if computer is scissors
+    return player wins
+if player is paper 
+    if computer is rock
+    return player wins
+    if else computer is paper
+    return 'tie'
+    if else computer is scissors 
+    return player wins
+if player is scissors 
+    if computer is rock 
+    reutrn player loses
+    if computer is paper
+    return player wins
+    if computer is scissors 
+    return 'tie' 
+*/
